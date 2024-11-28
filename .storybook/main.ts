6078e11c2d060cae431c80305d96c8f020101545
Giang Nguyen Truong <giang.nguyen@asnet.com.vn>
2024-11-28 12:29:53 +0700

    Feature: Create the BaseModal component

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,6 +8,7 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
+    '@storybook/preview-api',
   ],
   framework: {
     name: '@storybook/nextjs',
---
+++ src/components/common/BaseModal/index.stories.tsx
@@ -0,0 +1,60 @@
+import { useArgs } from '@storybook/preview-api';
+import type { Meta, StoryObj } from '@storybook/react';
+
+// Components
+import { BaseModal, Button } from '@/components';
+
+const meta = {
+  title: 'Components/BaseModal',
+  component: BaseModal,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    children: {
+      description: 'The modal content',
+    },
+    isOpen: {
+      description: 'The open state of the modal',
+      control: 'boolean',
+    },
+    onClose: {
+      description: 'The function to close the modal',
+      action: 'Closed the modal',
+    },
+    placement: {
+      description: 'The placement of the modal',
+    },
+  },
+} satisfies Meta<typeof BaseModal>;
+
+export default meta;
+
+type Story = StoryObj<typeof BaseModal>;
+
+export const Default: Story = {
+  render: (args) => {
+    const [isOpen, setIsOpen] = useArgs<typeof args>();
+
+    const handleOpen = () => setIsOpen({ isOpen: true });
+    const handleClose = () => setIsOpen({ isOpen: false });
+
+    return (
+      <>
+        <Button className="text-text-primary" onPress={handleOpen}>
+          Open Modal
+        </Button>
+        <BaseModal
+          {...args}
+          hideCloseButton={false}
+          isOpen={isOpen.isOpen}
+          onClose={handleClose}
+        />
+      </>
+    );
+  },
+  args: {
+    children: <div className="p-6">This is the modal content</div>,
+  },
+};
