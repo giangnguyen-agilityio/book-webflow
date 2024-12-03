2c39770c763cfa19d882d6d5eb0b4ce4082e3d91
Giang Nguyen Truong <giang.nguyen@asnet.com.vn>
2024-12-03 18:56:53 +0700

    fix: update the name for the route segment

---
+++ src/app/(workspace)/store/page.tsx
@@ -0,0 +1,61 @@
+import type { Metadata } from 'next';
+import { Suspense } from 'react';
+
+// Mocks
+import { MOCK_ARTICLE_LIST, MOCK_BOOK_LIST } from '@/mock';
+
+// Types
+// import { SearchParams } from '@/types';
+
+// UI components
+import { ArticlesAndResources, BookList, NewsletterSignup } from '@/ui';
+
+// Components
+import {
+  ArticlesAndResourcesSkeleton,
+  Banner,
+  BookListSkeleton,
+} from '@/components';
+
+export const metadata: Metadata = {
+  title: 'Store',
+  description: 'Browse and shop our wide range of products.',
+  openGraph: {
+    title: 'Store | Book WebFlow',
+    description: 'Browse and shop our wide range of products.',
+  },
+};
+
+// type HomepageSearchParamsProps = SearchParams;
+
+const Homepage = async () =>
+  // TODO: Add the search parameters to the props
+  // {searchParams}: {searchParams?: HomepageSearchParamsProps}
+  {
+    return (
+      <>
+        <Banner
+          metadataDescription={metadata.description}
+          metadataTitle={metadata.title}
+        />
+
+        <Suspense
+          // TODO: Add key from the searchParams for the suspense to show the fallback correctly
+          //  key={page + query}
+          fallback={<BookListSkeleton />}
+        >
+          {/* TODO: Will pass the searchParams into the component to fetch the data. */}
+          <BookList bookList={MOCK_BOOK_LIST} />
+        </Suspense>
+
+        <Suspense fallback={<ArticlesAndResourcesSkeleton />}>
+          {/* TODO: Will pass the searchParams into the component to fetch the data. */}
+          <ArticlesAndResources articles={MOCK_ARTICLE_LIST.slice(0, 3)} />
+        </Suspense>
+
+        <NewsletterSignup />
+      </>
+    );
+  };
+
+export default Homepage;
