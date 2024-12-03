8230de839bc72dd03a2b6015dc45c2edfcc228f0
Giang Nguyen Truong <giang.nguyen@asnet.com.vn>
2024-12-03 10:04:28 +0700

    Feature: Create the Not Found page

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,7 +1,12 @@
 'use client';
 
 import Link from 'next/link';
-import { NavbarMenu, NavbarMenuItem } from '@nextui-org/react';
+import {
+  NavbarMenu,
+  NavbarMenuItem,
+  useNavbarContext,
+} from '@nextui-org/react';
+import { cn } from '@nextui-org/theme';
 
 // Constants
 import { NAVIGATION_ITEMS, SOCIAL_LINK_ITEMS } from '@/constants';
@@ -9,30 +14,52 @@ import { NAVIGATION_ITEMS, SOCIAL_LINK_ITEMS } from '@/constants';
 // Components
 import { SocialLinks } from '@/components';
 
-const Sidebar = () => (
-  <NavbarMenu as="aside" className="sidebar" data-testid="sidebar">
-    {/* Navigation Items */}
-    {NAVIGATION_ITEMS.MAIN.map(({ url, label, title }) => (
-      <NavbarMenuItem
-        key={label}
-        className="transition-colors text-text-default font-semibold hover:text-text-tertiary hover:underline"
-        title={title}
-      >
-        <Link className="w-full" href={url}>
-          {label}
-        </Link>
-      </NavbarMenuItem>
-    ))}
+const Sidebar = () => {
+  const { setIsMenuOpen } = useNavbarContext();
 
-    {/* Social Network Links */}
-    <ul className="flex gap-2.5">
-      {SOCIAL_LINK_ITEMS.map(({ url, icon: Icon, title }) => (
-        <li key={title} className="text-text-primary">
-          <SocialLinks key={title} Icon={Icon} title={title} url={url} />
-        </li>
+  const handleCloseSidebar = () => {
+    setTimeout(() => {
+      setIsMenuOpen(false);
+    }, 150);
+  };
+
+  return (
+    <NavbarMenu
+      as="aside"
+      data-testid="sidebar"
+      className={cn(
+        'sidebar',
+        'bg-gradient-to-b from-background-primary to-background-primary/80',
+        'data-[menu-open=true]:backdrop-blur-none',
+      )}
+      onClick={handleCloseSidebar}
+    >
+      {/* Navigation Items */}
+      {NAVIGATION_ITEMS.MAIN.map(({ url, label, title }) => (
+        <NavbarMenuItem
+          key={label}
+          title={title}
+          className={cn(
+            'transition-colors text-text-default font-semibold',
+            'hover:text-text-tertiary hover:underline',
+          )}
+        >
+          <Link className="w-full" href={url}>
+            {label}
+          </Link>
+        </NavbarMenuItem>
       ))}
-    </ul>
-  </NavbarMenu>
-);
+
+      {/* Social Network Links */}
+      <li className="flex gap-2.5">
+        {SOCIAL_LINK_ITEMS.map(({ url, icon: Icon, title }) => (
+          <span key={title} className="text-text-primary">
+            <SocialLinks key={title} Icon={Icon} title={title} url={url} />
+          </span>
+        ))}
+      </li>
+    </NavbarMenu>
+  );
+};
 
 export default Sidebar;
